f93a1c60adc43c5d51fa47bb37c05a2a8fb6cad0
Harshit Joshi <Harshitrocky9@gmail.com>
2023-04-11 21:24:45 +0530

    add Background Change dir in Practice

---
+++ Practice/Background Change/script.js
@@ -0,0 +1,12 @@
+var buttonClick = document.getElementById('btn-click');
+var bgdiv = document.querySelector('main div');
+
+
+function bgcolor() {
+    buttonClick.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")"
+    bgdiv.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")"
+}
+
+
+
+buttonClick.onclick = bgcolor;
